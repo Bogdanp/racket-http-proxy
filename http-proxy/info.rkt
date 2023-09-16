2737857fcb015b477863719b1cf9f16896791e5d
Bogdan Popa <bogdan@defn.io>
2023-09-16 09:09:22 +0300

    build: add missing deps

--- http-proxy/info.rkt
+++ http-proxy/info.rkt
@@ -4,5 +4,7 @@
 (define collection "net")
 (define deps '("base"
                "http-proxy-lib"))
+(define build-deps '("racket-doc"
+                     "scribble-lib"))
 (define implies '("http-proxy-lib"))
 (define scribblings '(("http-proxy-manual.scrbl")))
